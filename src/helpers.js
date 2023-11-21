98cae2416f5dafe37cfd9a8f5acf3a787534a5d1
apterousteen <ayzhan312@live.ru>
2023-11-22 02:13:26 +0500

    handling error data from api

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,18 @@
 export const calcAverage = (arr) =>
-  Math.round(arr.reduce((acc, cur, _, arr) => acc + cur / arr.length, 0));
+  Math.round(
+    arr.reduce((acc, cur, _, arr) => {
+      const checkedCur = Number.isFinite(cur) ? cur : 0;
+      return acc + checkedCur / arr.length;
+    }, 0)
+  );
 
-export const fieldExists = (field) => field !== 'N/A';
+export const fieldExists = (field) => {
+  return field !== 'N/A' && field !== 'NaN' && !Number.isNaN(field);
+};
+
+export const handleImgError = (event, hideImg = false) => {
+  event.currentTarget.src =
+    'https://img.icons8.com/ios-filled/50/adb5bd/movie.png';
+
+  if (hideImg) event.currentTarget.className = 'imgHidden';
+};

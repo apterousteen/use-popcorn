660f2838bba643643975dfe632b31e803f56c0ac
apterousteen <ayzhan312@live.ru>
2023-11-21 20:33:54 +0500

    fetch movie on select render it on the right side

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { useEffect, useState } from 'react';
 import { tempMovieData, tempWatchedData } from './tempData';
-import { calcAverage } from './helpers';
+import { calcAverage, fieldExists } from './helpers';
 import StarRating from './StarRating';
 
-// TODO: pagination
+// TODO: infinite scroll ?
 const API_URL = 'https://www.omdbapi.com/?apikey=6e1f94e4';
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
   const [isLoading, setIsLoading] = useState(false);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState('matrix');
   const [errorMsg, setErrorMsg] = useState('');
+  const [selectedId, setSelectedId] = useState(null);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -52,6 +53,14 @@ export default function App() {
     return () => clearTimeout(delayDebounce);
   }, [query]);
 
+  const handleSelectMovie = (id) => {
+    setSelectedId((selectedId) => (selectedId === id ? null : id));
+  };
+
+  const handleCloseMovie = () => {
+    setSelectedId(null);
+  };
+
   return (
     <>
       <Nav>
@@ -62,13 +71,25 @@ export default function App() {
       <Main>
         <Container>
           {isLoading && <Loader />}
-          {!isLoading && !errorMsg && <MovieList movies={movies} />}
+          {!isLoading && !errorMsg && (
+            <MovieList
+              movies={movies}
+              selectedId={selectedId}
+              onSelect={handleSelectMovie}
+            />
+          )}
           {errorMsg && <ErrorMsg message={errorMsg} />}
         </Container>
 
         <Container>
-          <WatchedSummary watched={watched} />
-          <WatchedMovieList watched={watched} />
+          {selectedId ? (
+            <MovieDetails selectedId={selectedId} onClose={handleCloseMovie} />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList watched={watched} />
+            </>
+          )}
         </Container>
       </Main>
     </>
@@ -145,22 +166,31 @@ function Container({ children }) {
   );
 }
 
-function MovieList({ movies }) {
+function MovieList({ movies, onSelect, selectedId }) {
   return (
-    <ul className="list">
+    <ul className="list list-movies">
       {movies?.map((m) => (
-        <Movie movie={m} key={m.imdbID} />
+        <Movie
+          movie={m}
+          selectedId={selectedId}
+          onSelect={onSelect}
+          key={m.imdbID}
+        />
       ))}
     </ul>
   );
 }
 
-function Movie({ movie }) {
+function Movie({ movie, selectedId, onSelect }) {
   return (
-    <li key={movie.imdbID}>
+    <li
+      className={selectedId === movie.imdbID ? 'selected' : null}
+      onClick={() => onSelect(movie.imdbID)}
+      key={movie.imdbID}
+    >
       <img
         src={
-          movie.Poster !== 'N/A'
+          fieldExists(movie.Poster)
             ? movie.Poster
             : 'https://img.icons8.com/ios-filled/50/adb5bd/movie.png'
         }
@@ -174,6 +204,108 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
+  );
+}
+
+function MovieDetails({ selectedId, onClose }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  // TODO: set rating
+  const [rating, setRating] = useState(0);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(() => {
+    const fetchMovieDetailsUsingId = async () => {
+      try {
+        setIsLoading(true);
+        setErrorMsg('');
+        const res = await fetch(`${API_URL}&i=${selectedId}`);
+
+        if (!res.ok) throw new Error('Something went wrong');
+
+        const data = await res.json();
+
+        if (data.Error) throw new Error(data.Error);
+
+        setMovie(data);
+      } catch (e) {
+        console.error(e.message);
+        setErrorMsg(e.message);
+        setMovie({});
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (!selectedId) {
+      return;
+    }
+
+    fetchMovieDetailsUsingId();
+  }, [selectedId]);
+
+  return (
+    <div className="details">
+      {isLoading && <Loader />}
+      {errorMsg && <ErrorMsg message={errorMsg} />}
+      {!isLoading && !errorMsg && (
+        <>
+          <button title="Close movie" className="btn-back" onClick={onClose}>
+            &larr;
+          </button>
+          <header>
+            {fieldExists(poster) && <img src={poster} alt="poster" />}
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released}
+                {fieldExists(runtime) && (
+                  <>
+                    <span> &bull; </span> {runtime}
+                  </>
+                )}
+              </p>
+              {fieldExists(genre) && <p>{genre}</p>}
+              <p>
+                ⭐{' '}
+                {fieldExists(imdbRating)
+                  ? `${imdbRating} IMDb rating`
+                  : `No IMDb rating`}
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              <StarRating
+                maxRating={10}
+                starSize={24}
+                onSetRating={setRating}
+              />
+              {rating > 0 && <button className="btn-add">+ Add to list</button>}
+            </div>
+            <p>
+              <em>{fieldExists(plot) ? plot : 'No description'}</em>
+            </p>
+            {fieldExists(actors) && <p>Starring {actors}</p>}
+            {fieldExists(director) && <p>Directed by {director}</p>}
+          </section>
+        </>
+      )}
+    </div>
   );
 }
 
--- src/helpers.js
+++ src/helpers.js
@@ -1,2 +1,4 @@
 export const calcAverage = (arr) =>
   Math.round(arr.reduce((acc, cur, _, arr) => acc + cur / arr.length, 0));
+
+export const fieldExists = (field) => field !== 'N/A';
